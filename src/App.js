c98441515e49db1e57a2d507b503ac9e71063e5e
seun jay <seunjay92@gmail.com>
2019-10-14 14:41:53 +0100

    import Navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Index from "./components/layouts/Index";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
-
-
 
 export default App;

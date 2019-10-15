c0bd0c3fc91d72325aa9a899cc51f7496af66a29
seun jay <seunjay92@gmail.com>
2019-10-15 15:40:51 +0100

    Tracks component

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getTracks } from "../../actions/tracks";
+import Track from "./Track";
+import Spinner from "../spinner/Spinner";
 
-const Tracks = () => {
+const Tracks = ({
+  tracks: { trackList, fetching, fetched, heading },
+  getTracks
+}) => {
+  useEffect(() => {
+    const fetchTracks = async () => {
+      await getTracks();
+    };
+
+    fetchTracks();
+  }, [getTracks]);
+
+
   return (
-    <div>
-      <h1>Tracks</h1>
-    </div>
-  )
-}
+    <>
+      {fetching && <Spinner />}
+      {fetched && (
+        <>
+          <h3 className="text-center mb-5">{heading}</h3>
+          <div className="row">
+            {trackList.map((track, i) => (
+              <Track key={track.track.track_id} track={track.track} />
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
-export default Tracks
+const mapStateToProps = ({ tracks }) => {
+  return {
+    tracks
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getTracks }
+)(Tracks);

540b02864abf52dc8d2a40da2a3897fcdc771c07
seun jay <seunjay92@gmail.com>
2019-10-17 14:12:01 +0100

    Index component

--- src/components/layouts/Index.js
+++ src/components/layouts/Index.js
@@ -1,12 +1,53 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { connect } from "react-redux";
+import { getTracks, setSearchField, searchTracks } from "../../actions/tracks";
 import Tracks from "../tracks/Tracks";
+import Search from "../tracks/Search";
 
-const Index = () => {
+const Index = ({
+  tracks,
+  getTracks,
+  onSearchChange,
+  search: { searchField },
+  searchTracks
+}) => {
+  useEffect(() => {
+    const fetchTracks = async () => {
+      await getTracks();
+    };
+
+    fetchTracks();
+  }, [getTracks]);
+
+  const findTracks = e => {
+    e.preventDefault();
+    searchTracks(searchField);
+  };
+
   return (
     <Fragment>
-      <Tracks />
+      <Search handleChange={onSearchChange} findTracks={findTracks} />
+      <Tracks tracks={tracks} />
     </Fragment>
   );
 };
 
-export default Index;
+const mapStateToProps = ({ tracks, search }) => {
+  return {
+    tracks,
+    search
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getTracks: () => dispatch(getTracks()),
+    searchTracks: trackName => dispatch(searchTracks(trackName)),
+    onSearchChange: value => dispatch(setSearchField(value))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Index);

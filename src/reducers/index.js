8736ca07eb74b609c1f797ec4382a8eb58257a07
seun jay <seunjay92@gmail.com>
2019-10-17 14:11:47 +0100

    import search reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import tracks from "./tracks";
 import lyrics from "./lyrics";
+import search from "./search";
 
 export default combineReducers({
   tracks,
-  lyrics
+  lyrics,
+  search
 });

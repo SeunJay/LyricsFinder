3abcca3bd8d93672609f0d7569600d114d55f13c
seun jay <seunjay92@gmail.com>
2019-10-17 14:07:07 +0100

    actions

--- src/actions/tracks.js
+++ src/actions/tracks.js
@@ -1,8 +1,18 @@
-import { GET_TRACKS_FULFILLED, GET_TRACKS_PENDING } from "./types";
+import {
+  GET_TRACKS_FULFILLED,
+  GET_TRACKS_PENDING,
+  CHANGE_SEARCH_FIELD,
+  SEARCHED_TRACKS_FULFILLED
+} from "./types";
 import axios from "axios";
 
 const fetchTracks = payload => ({
   type: GET_TRACKS_FULFILLED,
+  payload
+});
+
+export const setSearchField = payload => ({
+  type: CHANGE_SEARCH_FIELD,
   payload
 });
 
@@ -15,6 +25,21 @@ export const getTracks = () => dispatch => {
     .then(res => {
       dispatch(fetchTracks(res.data.message.body.track_list));
       console.log(res.data.message.body.track_list);
+    })
+    .catch(err => console.log(err));
+};
+
+
+
+export const searchTracks = (trackName) => dispatch => {
+  dispatch({ type: GET_TRACKS_PENDING });
+  return axios
+    .get(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${trackName}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+    )
+    .then(res => {
+      dispatch(fetchTracks(res.data.message.body.track_list));
+      dispatch({ type: SEARCHED_TRACKS_FULFILLED });
     })
     .catch(err => console.log(err));
 };
